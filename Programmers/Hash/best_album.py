0fb887ef0c1462284a7a794bf9aec39b2ce1bc09
Min Oh <ody1205@gmail.com>
2020-01-26 03:26:31 +0900

    0126_2020

--- Programmers/Hash/best_album.py
+++ Programmers/Hash/best_album.py
@@ -1,34 +1,27 @@
-a = ['classic', 'pop', 'classic', 'classic', 'pop', 'abc']
-b = [500, 600, 150, 800, 2500, 10000]
-# should return 4, 1, 3, 0
+a = ['classic', 'pop', 'classic', 'classic', 'pop']
+b = [500, 600, 150, 800, 2500]
+# should return [4, 1, 3, 0]
 
 def solution(a,b):
-    if len(a) < 1:
-        return []
-    elif len(a) == 1:
-        return [0]
-    total = {}
+    num = [i for i in range(len(a))]
+    a = list(zip(a,b, num))
+    s = {}
     counter = {}
-    for i in range(len(a)):
-        if a[i] not in total:
-            total[a[i]] = b[i]
-            counter[a[i]] = 0
+    for i in a:
+        if i[0] not in s:
+            s[i[0]] = i[1]
+            counter[i[0]] = 0
         else:
-            total[a[i]] += b[i]
-        a[i] = [a[i],b[i],i]
-
-    a.sort(key=lambda x:(x[0],x[1]), reverse = True)
-    total = sorted(total, key = total.get, reverse = True)
-
-
+            s[i[0]] += i[1]
+    a.sort(reverse = True, key = lambda x: (x[1]))
+    s = sorted(s.items(), reverse = True, key = lambda x: x[1])
     answer = []
-    for i in total:
+    for i in s:
         for j in a:
-            if counter[i] < 2 and i == j[0]:
+            if counter[i[0]] < 2 and i[0] == j[0]:
                 answer.append(j[2])
-                counter[i] += 1
-    
+                counter[i[0]] += 1
+
     return answer
 
 print(solution(a,b))
-
