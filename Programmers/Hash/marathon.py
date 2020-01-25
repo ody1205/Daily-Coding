3faa683c1990fdc551c8d86a5f875ccc2ece89e7
Min Oh <ody1205@gmail.com>
2020-01-26 01:36:36 +0900

    0126_2020

--- Programmers/Hash/marathon.py
+++ Programmers/Hash/marathon.py
@@ -1,15 +1,22 @@
 a = ['leo', 'kiki', 'eden']
 b = ['eden', 'kiki']
 # should return leo
-c = ['mislav', 'stanko', 'mislav', 'ana']
-d = ['stanko', 'ana', 'mislav']
-# should return mislav
 
-import collections
-
-def solution(a, b):
-    c = collections.Counter(a) - collections.Counter(b)
-    return ''.join(list(c))
+def solution(a,b):
+    s = {}
+    # count everyone in the marathon
+    for i in a:
+        if i not in s:
+            s[i] = 1
+        else:
+            s[i] += 1
+    # subtract everyone who finished the marathon from total count
+    for i in b:
+        if i in s:
+            s[i] -= 1
+    # return who didn't finish the marathon
+    for i in s:
+        if s[i] == 1:
+            return i
 
 print(solution(a,b))
-print(solution(c,d))
