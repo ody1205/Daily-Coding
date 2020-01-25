d655e50ecc7b2535967c606be40907b5df0ddb9b
Min Oh <ody1205@gmail.com>
2020-01-26 02:51:12 +0900

    0126_2020

--- Programmers/Hash/spy.py
+++ Programmers/Hash/spy.py
@@ -1,26 +1,21 @@
 a = [['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]
-# should return 3 + 2 + 0 + 0 + 0 = 5
+# should return 5
 b = [['crow_mask', 'face'], ['blue_sunglasses', 'face'], ['smoky_makeup', 'face']]
-#should return 3 + 0 + 0 + 0 + 0 = 3
-c = [['crow_mask', 'face'], ['blue_sunglasses', 'eyewear'], ['yellow_hat', 'headgear']]
-#should return 3 + 1 + 1 + 1 + 1 = 7
 def solution(a):
-    a.sort(key = lambda x: x[1])
-    category = {}
-    result = 1
-    for i in range(len(a)):
-        if a[i][1] not in category:
-            category[a[i][1]] = 1
+    s = {}
+    for i in a:
+        if i[1] not in s:
+            s[i[1]] = 1
         else:
-            category[a[i][1]] += 1
+            s[i[1]] += 1
+    answer = 1
+    for i in s:
+        s[i] += 1
     
-    for i in category:
-        result *= category[i] + 1
+    for i in s:
+        answer *= s[i]
 
-
-    return result - 1
+    return answer - 1
 
 print(solution(a))
 print(solution(b))
-print(solution(c))
-
