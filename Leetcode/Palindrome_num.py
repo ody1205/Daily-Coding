a0af94fe78464bfce5f1abd0dd5d2a68c306cd30
Min Oh <ody1205@gmail.com>
2020-02-10 23:37:46 +0900

    0210_2020

--- Leetcode/Palindrome_num.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        a = list(str(x))[::-1]
-        b = ''.join(a)
-        if str(x) == b:
-            return True
-        else:
-            return False
