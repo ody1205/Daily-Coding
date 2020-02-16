8bb1c83ad11c939f8063024e1266478b9484064f
Min Oh <ody1205@gmail.com>
2020-02-17 03:57:57 +0900

    0217_2020

--- Leetcode/top_100_liked_problems/17.py
+++ Leetcode/top_100_liked_problems/17.py
@@ -19,24 +19,15 @@ class Solution:
                  '7': ['p', 'q', 'r', 's'],
                  '8': ['t', 'u', 'v'],
                  '9': ['w', 'x', 'y', 'z']}
-                
-        def backtrack(combination, next_digits):
-            # if there is no more digits to check
-            if len(next_digits) == 0:
-                # the combination is done
+        def backtracking(combination, digits):
+            print(combination)
+            if len(digits) == 0:
                 output.append(combination)
-            # if there are still digits to check
             else:
-                # iterate over all letters which map 
-                # the next available digit
-                for letter in phone[next_digits[0]]:
-                    # append the current letter to the combination
-                    # and proceed to the next digits
-                    backtrack(combination + letter, next_digits[1:])
-                    
+                for i in phone[digits[0]]:
+                    backtracking(combination + i, digits[1:])
         output = []
         if digits:
-            backtrack("", digits)
+            backtracking('', digits)
         return output
-
-print(Solution.letterCombinations(Solution, '233'))
+print(Solution.letterCombinations(Solution, '23'))
